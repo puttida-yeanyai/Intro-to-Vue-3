9143def9efac1130257c635f9acefc2f1e8200b2
focuskawisara <kawisara.nak@gmail.com>
2022-03-30 10:46:39 +0700

    git checkout L4-start

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            Image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
+        }
+    }
+})
